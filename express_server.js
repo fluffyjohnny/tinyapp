e725a0a13e930c7036462178e7db48da4d03e745
John Chia <john.chia0106@gmail.com>
2022-01-24 17:55:21 -0800

    added a new url_new file into the views folder, and implemented a random string generator

--- express_server.js
+++ express_server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = () => {
+  let str = '';
+  const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 7; i++) {
+    str += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return str;
 };
 
 app.get("/", (req, res) => {
@@ -17,14 +28,14 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVar);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
-
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -37,6 +48,11 @@ app.get("/hello", (req, res) => {
 app.get("*", (req, res) => {
   res.status(404).write('404 Page Not Found!');
   res.end();
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
