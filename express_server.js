db4ee31a1102e17ec7a74a2d6174beb779e6d045
John Chia <john.chia0106@gmail.com>
2022-01-25 15:02:41 -0800

    implemented a delete button with a post route that deletes urls from the database

--- express_server.js
+++ express_server.js
@@ -45,9 +45,19 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log('req.param', req.params);
+  const index = req.params['shortURL'];
+  delete urlDatabase[index];
+  res.redirect('/urls');
 });
 
 
@@ -57,7 +67,6 @@ app.post("/urls", (req, res) => {
   console.log(req.body);
   res.redirect(`/urls/${generatedURL}`);
 });
-
 
 
 app.listen(PORT, () => {
