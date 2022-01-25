3a9fbde7550abf53fe277c30dbc3f3213a031110
John Chia <john.chia0106@gmail.com>
2022-01-24 16:37:56 -0800

    added the header partial and changed the view files

--- express_server.js
+++ express_server.js
@@ -18,8 +18,10 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
+
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+
 });
 
 
