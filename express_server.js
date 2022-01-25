4bf771b5af458eac1473b082a5302343fedfd3bd
John Chia <john.chia0106@gmail.com>
2022-01-24 16:11:23 -0800

    added two routes and templates for /urls and /urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -17,6 +17,13 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVar);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
+});
+
+
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
