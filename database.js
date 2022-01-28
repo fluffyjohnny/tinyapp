963600c211231228d9a80609e5d5f8e1c08723dc
John Chia <john.chia0106@gmail.com>
2022-01-28 13:59:51 -0800

    refactored the database into a seperate file

---
+++ database.js
@@ -0,0 +1,38 @@
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: 'example',
+    time: 'Jan/6/2021',
+    uniquePool: [],
+    logsUser: [],
+    logsTime: [],
+    uniqueClicks: 0,
+    clicks: 0,
+
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: 'example',
+    time: 'Jan/6/2021',
+    uniquePool: [],
+    logsUser: [],
+    logsTime: [],
+    uniqueClicks: 0,
+    clicks: 0,
+  },
+};
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+module.exports = { urlDatabase, users };
--- express_server.js
+++ express_server.js
@@ -21,43 +21,7 @@ app.use(cookieSession({
 
 // ------------------------------------ Data -----------------------------------------
 
-const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: 'example',
-    time: 'Jan/6/2021',
-    uniquePool: [],
-    logsUser: [],
-    logsTime: [],
-    uniqueClicks: 0,
-    clicks: 0,
-
-  },
-  "9sm5xK": {
-    longURL: "http://www.google.com",
-    userID: 'example',
-    time: 'Jan/6/2021',
-    uniquePool: [],
-    logsUser: [],
-    logsTime: [],
-    uniqueClicks: 0,
-    clicks: 0,
-  },
-};
-
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
-
+const { urlDatabase, users } = require('./database');
 
 // ----------------------------- Helper Functions ------------------------------
 
